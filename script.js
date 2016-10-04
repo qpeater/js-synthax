48a548b0bf56051a67049622f05502fdfc01cf12
Sergey Fedorov <sfedorov@octoberry.ru>
2016-10-04 21:11:46 +0300

    date

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for (var a = 1; a<=100; a++){
     }
 }
 
+
 var age = 50,
     a = 17,
     b = 18,
@@ -17,4 +18,22 @@ else if ( age >= b && age < c ) {
 }
 else {
     console.log('привет, бабуля!');
+}
+
+
+var date = '22.03';
+
+switch (date){
+    case "08.03":console.log('с 8 марта!');
+
+        break;
+    case '23.02':console.log('с 23 февраля');
+
+        break;
+    case '31.12':console.log('с новым годом');
+
+        break;
+    default: console.log('сегодня просто отличный день и безо всякого праздника');
+
+        break;
 }
